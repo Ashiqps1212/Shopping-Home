4036e8aba06acc5eee6dda761b616bc357126403
ASHIQ PS <ashiqps97@gmail.com>
2019-10-03 14:10:31 +0530

    First Commit is done

---
+++ ShoppingHomeBackend/src/main/java/aqz/project/ShoppingHomeBackend/App.java
@@ -0,0 +1,13 @@
+package aqz.project.ShoppingHomeBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
